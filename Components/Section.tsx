9c8cccf8630713affc29991410796f6c439a7dfb
Sergio <sergio.torrez@serez.dev>
2026-09-21 15:03:13 -0300

    fix style in sections improve

--- Components/Section.tsx
+++ Components/Section.tsx
@@ -3,8 +3,8 @@ import { SectionsType } from "../types/Elements"
 export default function Section({ title, children }: SectionsType) {
 
     return (
-        <div className="rounded-2xl border border-white/10 bg-[#12141e] px-15 py-4 shadow-xl max-w-lg w-full h-35">
-            <div className="flex lg:flex-row sm:flex-col justify-between items-start">
+        <div className="rounded-2xl border border-white/10 bg-[#12141e] px-15 py-4 shadow-xl max-w-lg w-full lg:h-35 mb:h-48">
+            <div className="flex flex-row justify-between items-start">
                 <h2 className="text-lg font-semibold text-white mb-4 mr-4">{title}</h2>
             </div>
             <div>
